96394190110d0153a232aef6fdbe25b6d6469574
erikXavier122 <breno.xavier@eurekalabs.com.br>
2023-07-05 11:48:47 -0300

    alteracao controler

--- src/main/java/com/springboot/empresa/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/springboot/empresa/service/impl/CompanyServiceImpl.java
@@ -2,8 +2,11 @@ package com.springboot.empresa.service.impl;
 
 import com.springboot.empresa.domain.model.v1.CompanyModel;
 import com.springboot.empresa.domain.repository.v1.CompanyRepository;
+import com.springboot.empresa.dto.v1.CompanyDto;
 import com.springboot.empresa.service.CompanyService;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -49,6 +52,29 @@ public class CompanyServiceImpl  implements CompanyService {
     @Override
     public void delete(CompanyModel companyModel) {
 
+    }
+
+    @Override
+    public Object updateById(UUID id, CompanyDto companyDto) {
+        Optional<CompanyModel> companyModelOptional = findById(id);
+        if (companyModelOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nao temos cadastro dessa empresa para atualizar.");
+        }
+        var companyModel = companyModelOptional.get();
+        companyModel.setNome_empresa(companyDto.getNome_empresa());
+        companyModel.setCnpj(companyDto.getCnpj());
+        companyModel.setCep(companyDto.getCep());
+        return "Cadastro da empresa atualizado";
+    }
+
+    @Override
+    public Object deleteById(UUID id) {
+        Optional<CompanyModel> companyModelOptional = findById(id);
+        if (!companyModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cadastro dessa empresa nao existe!");
+        }
+        delete(companyModelOptional.get());
+        return "Cadastro da empresa apagado com sucesso.";
     }
 }
 
